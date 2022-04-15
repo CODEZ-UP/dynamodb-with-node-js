4027f7e91d7eeda93fb728cdde8dae4cd4a08776
Codez Up <codezup@gmail.com>
2022-04-16 02:34:04 +0530

    Integrate AWS DynamoDB with Node.js

---
+++ index.js
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv';
+import express from "express";
+import bodyParser from "body-parser";
+import users from "./routes/users"
+
+dotenv.config()
+const port = 3000
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+	res.json({ "hello": "world" })
+})
+
+app.use('/api', users );
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
